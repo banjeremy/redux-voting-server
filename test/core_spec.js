21953f60b4ad801ad9ef921e54ae723a01664e65
Jeremy Jones <banjeremy@gmail.com>
2016-02-18 14:46:33 -0600

    add next method

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -21,6 +21,26 @@ describe('application logic', () => {
 
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+  });
+
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+      )});
+
+      let nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
       }));
     });
   });
